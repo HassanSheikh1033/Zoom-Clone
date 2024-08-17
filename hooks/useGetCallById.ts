67807b2c2e78dba038f91202f4add7e733d94972
Huziafaf45 <153464118+HassanSheikh1033@users.noreply.github.com>
2024-08-17 15:04:33 +0500

    Initial commit

---
+++ hooks/useGetCallById.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
+
+export const useGetCallById = (id: string | string[]) => {
+  const [call, setCall] = useState<Call>();
+  const [isCallLoading, setIsCallLoading] = useState(true);
+
+  const client = useStreamVideoClient();
+
+  useEffect(() => {
+    if (!client) return;
+    
+    const loadCall = async () => {
+      try {
+        // https://getstream.io/video/docs/react/guides/querying-calls/#filters
+        const { calls } = await client.queryCalls({ filter_conditions: { id } });
+
+        if (calls.length > 0) setCall(calls[0]);
+
+        setIsCallLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsCallLoading(false);
+      }
+    };
+
+    loadCall();
+  }, [client, id]);
+
+  return { call, isCallLoading };
+};
+
+
+
+
+// The useStreamVideoClient hook is used to get an instance of the Stream Video client, which is required
+//  to interact with the Stream Video API.
+
+
+// The filter_conditions parameter in the queryCalls method of the Stream Video SDK allows you to specify 
+// various criteria to filter the calls you want to retrieve
